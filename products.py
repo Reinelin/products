9bb9e3a03a7b417129c1cc8ff6b353057e3c1496
Reinelin <yclinreine@gmail.com>
2020-04-11 23:44:06 +0800

    add reading

--- products.py
+++ products.py
@@ -1,3 +1,14 @@
+# Import data
+products = []
+with open('products.csv', 'r') as f:
+	for line in f:
+		if 'product,price' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name,price])
+print(products)
+
+# user input
 products = []
 while True:
 	name = input('please enter name of product:')
@@ -7,5 +18,13 @@ while True:
 	products.append([name, price])
 print(products)
 
+# print price of records
 for p in products:
 	print('the price of', p[0], 'is', p[1])
+
+# write data
+with open('products.csv', 'w') as f:
+	f.write('product,price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
